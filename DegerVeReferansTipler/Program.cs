04d401122a540bb89fe141b6e248ace8c551ef15
Mert <muhammedmertsiranun@gmail.com>
2024-03-15 23:29:34 +0300

    Add project files.

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int,decimal,float,double,bool = değer tip
+            //array,class,interface = referans tip
+
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+            //say1 ?? 30
+
+            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+            sayilar1 = sayilar2;
+            sayilar2[0]=999;
+            //sayilar1[0] ?? 999
+             
+            Console.WriteLine(sayi1);
+            Console.WriteLine(sayilar1[0]);
+        }
+    }
+}
---
+++ KampReborn/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+
+namespace KampReborn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güveliği 
+            // do not repeat yourself - Kendini tekrarlama
+            //değer tutucu, alias
+
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true ;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if(dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+
+            if (sistemeGirisYapmisMi==true)
+            {
+                Console.WriteLine("Sisteme girişiniz başaralıyla gerçekleşmiştir");
+            }
+
+            else
+            {
+                Console.WriteLine("Sisteme giriş işleminiz başarısız olmuştur");
+            }
+            Console.WriteLine(kategoriEtiketi);
+            
+
+        }
+    }
+}
