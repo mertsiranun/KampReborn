08e6a2d2c27687851d2b8627ff71af65f3f47168
Mert <muhammedmertsiranun@gmail.com>
2024-03-16 02:34:50 +0300

    16 mart

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //encapsulation
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi");
+        }
+
+        #region return ve void farkı
+        //public int Topla(int sayi1, int sayi2)
+        //{
+        //    return sayi1 + sayi2;
+        //}
+        //// void çıktıyı gösterirken, return değeri(sonucu) kullanmamızı sağlar örneğin
+        //// int toplamSonuc=productManager.Topla(3,6)
+        //// Console.WriteLine(toplamSonuc*2);
+        ////şeklinde işlemlerde kullanabiliriz ama bunu void'de yapamayız 
+        ////çünkü void çıktısını göstermekle mükelleftir.
+        //public void Topla2(int sayi1, int sayi2)
+        //{
+        //    Console.WriteLine(sayi1+sayi2);
+        //}
+        #endregion
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace OOP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1= new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product { Id=2, CategoryId=5,UnitsInStock=5, ProductName="Kalem", UnitPrice=35};
+
+            //PascalCase   //camelCase
+            //case sensetive
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+
+            //int, double, bool... değer tip
+            //diziler, class, abstract class, interface... referans tip
+
+            //productManager.Topla2(3, 6);
+
+            // void çıktıyı gösterirken, return değeri(sonucu) kullanmamızı sağlar örneğin
+            // int toplamSonuc=productManager.Topla(3,6)
+            // Console.WriteLine(toplamSonuc*2);
+            //şeklinde işlemlerde kullanabiliriz ama bunu void'de yapamayız 
+            //çünkü void çıktısını göstermekle mükelleftir.
+
+            //int toplamSonuc = productManager.Topla(3, 6);
+            //Console.WriteLine(toplamSonuc*2);
+
+            //Console.WriteLine(productManager.Topla(3,6) * 2); şeklinde de çalışır
+
+
+        }
+    }
+}
